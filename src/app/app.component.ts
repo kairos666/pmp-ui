9076c3d5f8675ccb32b615e922d53982c6effd09
David Maggi <david.maggi@orange.com>
2016-10-10 22:56:36 +0200

    first try to play with material2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 /* tslint:disable:no-unused-variable */
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MdSidenav } from "@angular/material";
 
 /* GLOBAL SERVICES */
 import { LocalStorageService } from './services/local-storage.service';
@@ -16,8 +17,26 @@ import { ConfigModelService } from './model/config-model.service';
 })
 export class AppComponent {
   private title = 'app works!';
+  private currentDog = {};
+  private dogs = [
+    {rows: 2, name: "Mal", human: "Jeremy", age: 5},
+    {rows: 1, name: "Molly", human: "David", age: 5},
+    { rows: 1, name: "Sophie", human: "Alex", age: 8},
+    {rows: 2, name: "Taz", human: "Joey", age: '11 weeks'},
+    { rows: 1, name: "Kobe", human: "Igor", age: 5},
+    {rows: 2, name: "Porter", human: "Kara", age: 3},
+    { rows: 1, name: "Stephen", human: "Stephen", age: 8},
+    {rows: 1, name: "Cinny", human: "Jules", age: 3},
+    { rows: 1, name: "Hermes", human: "Kara", age: 3},
+  ];
+  @ViewChild('sidenav') sidenav: MdSidenav;
 
   constructor (private configModel:ConfigModelService) {
     // instanciate app-wide dependencies for singleton injections
+  }
+
+  private showDog(dog) {
+    this.currentDog = dog;
+    this.sidenav.open();
   }
 }
