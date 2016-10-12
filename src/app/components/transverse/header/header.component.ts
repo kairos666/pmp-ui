d7a6a552220515db55ea02e952c19e999e07a27d
David Maggi <david.maggi@orange.com>
2016-10-12 14:33:25 +0200

    header implementation - engine status & action buttons

--- src/app/components/transverse/header/header.component.ts
+++ src/app/components/transverse/header/header.component.ts
@@ -1,14 +1,41 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, Input, OnInit } from '@angular/core';
+import { ConfigActions } from '../../../schema/config';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-  private status = 'started';
+export class HeaderComponent implements OnInit  {
+  private engineStatus:string;
+  private isConnected:boolean;
+  private isStartAllowed = true;
+  private isStopAllowed = true;
+  @Input() statusStream: Observable<string>;
+  @Input() connectionStream: Observable<boolean>;
+  @Input() availableActionsStream: Observable<ConfigActions>;
   @Output() onMenuCall = new EventEmitter();
+  @Output() onStartClick = new EventEmitter();
+  @Output() onStopClick = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
+  ngOnInit():void {
+    // handle available actions changes
+    this.availableActionsStream.subscribe(aActions => {
+      this.isStartAllowed = aActions.startAllowed;
+      this.isStopAllowed = aActions.stopAllowed;
+    });
+
+    // handle engine status changes
+    this.statusStream.subscribe(status => {
+      this.engineStatus = status;
+    });
+
+    // handle connection status changes
+    this.connectionStream.subscribe(isConnected => {
+      this.isConnected = isConnected;
+    });
+  }
 }
