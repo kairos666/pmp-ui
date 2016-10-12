add2308d1e93df79c3225567272a7d55485b42bd
David Maggi <david.maggi@orange.com>
2016-10-12 23:27:42 +0200

    help section implemented
    - text cards + styles
    - secondary nav general styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,25 +12,10 @@ import { ConfigModelService } from './model/config-model.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  private dogs = [
-    {rows: 2, name: "Mal", human: "Jeremy", age: 5},
-    {rows: 1, name: "Molly", human: "David", age: 5},
-    { rows: 1, name: "Sophie", human: "Alex", age: 8},
-    {rows: 2, name: "Taz", human: "Joey", age: '11 weeks'},
-    { rows: 1, name: "Kobe", human: "Igor", age: 5},
-    {rows: 2, name: "Porter", human: "Kara", age: 3},
-    { rows: 1, name: "Stephen", human: "Stephen", age: 8},
-    {rows: 1, name: "Cinny", human: "Jules", age: 3},
-    { rows: 1, name: "Hermes", human: "Kara", age: 3},
-  ];
   @ViewChild('sidenav') sidenav: MdSidenav;
 
   constructor (private configModel:ConfigModelService, private pmpEngine:PmpEngineConnectorService) {
     // instanciate app-wide dependencies for singleton injections
-  }
-
-  private showDog(dog) {
-    this.sidenav.open();
   }
 
   private mainNavSelection():void { this.sidenav.close(); }
--- src/app/components/views/dashboard/dashboard.component.ts
+++ src/app/components/views/dashboard/dashboard.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+  private dogs = [
+    {rows: 2, name: "Mal", human: "Jeremy", age: 5},
+    {rows: 1, name: "Molly", human: "David", age: 5},
+    { rows: 1, name: "Sophie", human: "Alex", age: 8},
+    {rows: 2, name: "Taz", human: "Joey", age: '11 weeks'},
+    { rows: 1, name: "Kobe", human: "Igor", age: 5},
+    {rows: 2, name: "Porter", human: "Kara", age: 3},
+    { rows: 1, name: "Stephen", human: "Stephen", age: 8},
+    {rows: 1, name: "Cinny", human: "Jules", age: 3},
+    { rows: 1, name: "Hermes", human: "Kara", age: 3},
+  ];
   constructor() { }
 
   ngOnInit() {
