8f7c28aa21c2e7f41700948457c283406123df70
David Maggi <david.maggi@orange.com>
2016-10-18 22:45:02 +0200

    dashboard layout + styles

--- src/app/components/views/dashboard/dashboard.component.ts
+++ src/app/components/views/dashboard/dashboard.component.ts
@@ -1,25 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { LogsService } from '../../../model/logs-model.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
-  private dogs = [
-    {rows: 2, name: 'Mal', human: 'Jeremy', age: 5},
-    {rows: 1, name: 'Molly', human: 'David', age: 5},
-    { rows: 1, name: 'Sophie', human: 'Alex', age: 8},
-    {rows: 2, name: 'Taz', human: 'Joey', age: '11 weeks'},
-    { rows: 1, name: 'Kobe', human: 'Igor', age: 5},
-    {rows: 2, name: 'Porter', human: 'Kara', age: 3},
-    { rows: 1, name: 'Stephen', human: 'Stephen', age: 8},
-    {rows: 1, name: 'Cinny', human: 'Jules', age: 3},
-    { rows: 1, name: 'Hermes', human: 'Kara', age: 3},
-  ];
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class DashboardComponent {
+  constructor(
+    private logsService:LogsService
+  ) {}
 }
