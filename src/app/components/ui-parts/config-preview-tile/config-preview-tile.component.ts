ec19c161e463cc67dcc3c304a13391138ea4fdaa
David Maggi <david.maggi@orange.com>
2016-11-06 08:06:37 +0100

    #8  switched data from UI config to engine config

--- src/app/components/ui-parts/config-preview-tile/config-preview-tile.component.ts
+++ src/app/components/ui-parts/config-preview-tile/config-preview-tile.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { Router }   from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { PimpConfig, PimpRule } from '../../../schema/config';
 
 @Component({
@@ -10,35 +10,46 @@ import { PimpConfig, PimpRule } from '../../../schema/config';
       <button type="button" class="btn-edit" (click)="onClickEdit()"><md-icon>mode_edit</md-icon></button>
       <span>Pimp rules</span>
     </h3>
-    <ul class="pimp-rules-tile-container item-list">
-      <li *ngFor="let i = index; let rule of pimpRulesStream | async">
-        <md-icon>invert_colors</md-icon>
-        <h4>Rule #{{i + 1}}</h4>
-        <p>{{rule.url}}</p>
-      </li>
-      <li *ngIf="hasNoRules">
+    <ul *ngIf="rules.length !== 0" class="pimp-rules-tile-container item-list">
+      <template ngFor let-rule let-i="index" [ngForOf]="rules">
+        <li>
+          <md-icon>invert_colors</md-icon>
+          <h4>Rule #{{i + 1}}</h4>
+          <p>{{rule.url}}</p>
+        </li>
+      </template>
+    </ul>
+    <ul class="no-data item-list" *ngIf="rules.length === 0">
+      <li>
         <md-icon>not_interested</md-icon>
-        <h4>No pimp rules</h4>
-        <p>this configuration has no rules applied</p>
+        <h4>No data</h4>
+        <p>Only available when pimp engine is started and has some rules defined</p>
       </li>
     </ul>
   `,
 })
-export class ConfigPreviewTileComponent implements OnInit {
+export class ConfigPreviewTileComponent implements OnInit, OnDestroy {
   @Input() config:Observable<PimpConfig>;
-  private pimpRulesStream:Observable<PimpRule[]>;
-  private hasNoRules = false;
+  private rules:PimpRule[];
+  private sub:Subscription;
 
   constructor(private router:Router) { }
 
   ngOnInit() {
-    this.pimpRulesStream = this.config.map(config => config.pimpCmds).do(pimpRules => {
-      // no rules case
-      if (pimpRules.length === 0) this.hasNoRules = true;
+    this.sub = this.config.subscribe(pimpRules => {
+      if(pimpRules) {
+        this.rules = pimpRules.pimpCmds;
+      } else {
+        this.rules = [];
+      }
     });
   }
 
   private onClickEdit() {
     this.router.navigate(['/configuration', { selectedTabIndex: 1 }]);
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 }
